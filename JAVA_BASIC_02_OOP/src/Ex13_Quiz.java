9ca113ec06bc102fdac28f5fd72bfd21237004f0
ChoiMari <c98639863@gmail.com>
2025-08-26 17:54:09 +0900

    feat : 생성자 퀴즈

---
+++ JAVA_BASIC_02_OOP/src/Ex13_Quiz.java
@@ -0,0 +1,36 @@
+/*
+  노트북
+  기본 사양 : OS - 윈도우
+  RAM : 8기가
+  SSD : 256기가
+  
+  옵션
+  고객에 따라서
+  운영체제, RAM, SSD 변경가능 
+ */
+
+class NoteBook{
+	private String os;
+	private int ram;
+	private int ssd;
+	
+	public NoteBook(){
+		os = "Window11";
+		ram = 8;
+		ssd = 256;
+	}
+	public NoteBook(String os) {
+		this.os = os;
+		ram = 8;
+		ssd = 256;
+	}
+}
+
+public class Ex13_Quiz {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
